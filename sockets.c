bf3368c77c29fbfc8dbae0d54f095a02fc761423
Jordan Heier <heierj@hannes.cs.washington.edu>
2014-10-06 11:42:41 -0700

    Working on creating header

--- sockets.c
+++ sockets.c
@@ -8,8 +8,34 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <iostream>
+#include "sockets.h"
+
+#define STUDENT_NUMBER 980
+#define HEADER_SIZE 12
 
 int main(int argc, char **argv) {
 
+}
+
+char *create_header(uint32_t payload_len, uint32_t psecret, uint16_t step) {
+  
+  void *header = (char *) malloc(HEADER_SIZE);
+  if(header == NULL) {
+    return NULL;
+  }
+
+  // Add data into the header converting from
+  // host order to network order
+  char *current_pos = header;
+  *current_pos = htonl(payload_len);
+  current_pos += 4;
+  *current_pos = htonl(psecret);
+  current_pos += 4;
+  *current_pos = htons(step);
+  current_pos += 2;
+
+  uint16_t s_number = STUDENT_NUMBER;
+  *current_pos = htons(s_number);
+
+  return header;
 }
