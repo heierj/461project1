843172031f30304e614457bc417cfe170a83beea
Jordan Heier <heierj@hannes.cs.washington.edu>
2014-10-06 11:01:58 -0700

    Set-up file for project

---
+++ sockets.c
@@ -0,0 +1,15 @@
+// Jordan Heier, Will McNamara, Cameron Hardin
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+}
---
+++ tcp.c
@@ -0,0 +1,13 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+}
